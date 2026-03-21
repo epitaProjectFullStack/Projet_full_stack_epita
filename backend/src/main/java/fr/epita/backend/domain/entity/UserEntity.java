a61b32917aef9de9b4bf1b4ddd0260178c7c64c3
SunnyBara <sunny.soyer@epita.fr>
2026-03-21 19:42:09 +0100

    feat: add UserService

---
+++ backend/src/main/java/fr/epita/backend/domain/entity/UserEntity.java
@@ -0,0 +1,16 @@
+package fr.epita.backend.domain.entity;
+
+import fr.epita.backend.utils.Role;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserEntity {
+    private String login;
+    private String password;
+    private String mail;
+    private Role role;
+    private String token;
+    private boolean banned;
+}
