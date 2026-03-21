cfe295bac482999c3459fbd89d0166cb54d7df08
SunnyBara <sunny.soyer@epita.fr>
2026-03-21 19:35:12 +0100

    feat: Add User model and repository

---
+++ backend/src/main/java/fr/epita/backend/data/model/UserModel.java
@@ -0,0 +1,44 @@
+package fr.epita.backend.data.model;
+
+import fr.epita.backend.utils.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserModel {
+
+    @Id
+    @UuidGenerator
+    @Column(columnDefinition = "uuid", nullable = false, updatable = false)
+    private UUID uuid;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password; // a Hasher
+
+    @Column(name = "mail_address", nullable = false, unique = true)
+    private String mail;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    @Column(name = "token", nullable = true)
+    private String token;
+
+    @Column(name = "banned", nullable = false)
+    private boolean banned;// A modifier en Objet comportant plus de détails Date du Ban / Raison du Ban /
+                           // Durée du Ban
+                           // Potentiellement un Enum Banned/Active ?
+}
---
+++ backend/src/main/java/fr/epita/backend/data/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package fr.epita.backend.data.repository;
+
+import fr.epita.backend.data.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    Optional<UserModel> findByLogin(String login);
+
+}
