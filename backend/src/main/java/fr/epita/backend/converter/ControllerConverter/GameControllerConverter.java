f79a37e25abf860b4dcabbea3bf60688e731f68d
SunnyBara <sunny.soyer@epita.fr>
2026-03-21 19:43:18 +0100

    feat: add Game domain and converter

---
+++ backend/src/main/java/fr/epita/backend/converter/ControllerConverter/GameControllerConverter.java
@@ -0,0 +1,76 @@
+package fr.epita.backend.converter.ControllerConverter;
+
+import fr.epita.backend.controller.api.request.GameRequest;
+import fr.epita.backend.controller.api.response.GameResponses.GameResponse;
+import fr.epita.backend.controller.api.response.GameResponses.GameVersionResponse;
+import fr.epita.backend.controller.api.response.GameResponses.GameVersionsResponse;
+import fr.epita.backend.controller.api.response.GameResponses.GamesResponse;
+import fr.epita.backend.domain.entity.GameArticleVersionEntity;
+import fr.epita.backend.domain.entity.GameEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GameControllerConverter {
+
+    public GameEntity fromRequestToEntity(GameRequest request) {
+        GameEntity entity = new GameEntity();
+        entity.setAuthorId(request.getAuthorId());
+        entity.setSubjectGameName(request.getSubjectGameName());
+        entity.setArticleName(request.getArticleName());
+        entity.setArticleContent(request.getArticleContent());
+        return entity;
+    }
+
+    public GameResponse fromEntityToResponse(GameEntity entity) {
+        GameResponse response = new GameResponse();
+        response.setUuid(entity.getUuid());
+        response.setAuthorId(entity.getAuthorId());
+        response.setAuthorLogin(entity.getAuthorLogin());
+        response.setSubjectGameName(entity.getSubjectGameName());
+        response.setArticleName(entity.getArticleName());
+        response.setArticleContent(entity.getArticleContent());
+        response.setVersion(entity.getVersion());
+        response.setCreatedAt(entity.getCreatedAt());
+        return response;
+    }
+
+    public GamesResponse fromEntitiesToResponses(List<GameEntity> entities) {
+        List<GameResponse> responses = new ArrayList<>();
+
+        for (GameEntity entity : entities) {
+            responses.add(fromEntityToResponse(entity));
+        }
+
+        GamesResponse wrapper = new GamesResponse();
+        wrapper.setList(responses);
+        return wrapper;
+    }
+
+    public GameVersionResponse fromVersionEntityToResponse(GameArticleVersionEntity entity) {
+        GameVersionResponse response = new GameVersionResponse();
+        response.setUuid(entity.getUuid());
+        response.setGameId(entity.getGameId());
+        response.setAuthorId(entity.getAuthorId());
+        response.setAuthorLogin(entity.getAuthorLogin());
+        response.setArticleName(entity.getArticleName());
+        response.setArticleContent(entity.getArticleContent());
+        response.setVersion(entity.getVersion());
+        response.setCreatedAt(entity.getCreatedAt());
+        return response;
+    }
+
+    public GameVersionsResponse fromVersionEntitiesToResponses(List<GameArticleVersionEntity> entities) {
+        List<GameVersionResponse> responses = new ArrayList<>();
+
+        for (GameArticleVersionEntity entity : entities) {
+            responses.add(fromVersionEntityToResponse(entity));
+        }
+
+        GameVersionsResponse wrapper = new GameVersionsResponse();
+        wrapper.setList(responses);
+        return wrapper;
+    }
+}
---
+++ backend/src/main/java/fr/epita/backend/converter/DataConverter/GameDataConverter.java
@@ -0,0 +1,43 @@
+package fr.epita.backend.converter.DataConverter;
+
+import fr.epita.backend.data.model.GameArticleVersionModel;
+import fr.epita.backend.data.model.GameModel;
+import fr.epita.backend.domain.entity.GameArticleVersionEntity;
+import fr.epita.backend.domain.entity.GameEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GameDataConverter {
+
+    public GameEntity fromModelToEntity(GameModel model) {
+        GameArticleVersionModel currentVersion = model.getCurrentVersion();
+
+        GameEntity entity = new GameEntity();
+        entity.setUuid(model.getUuid());
+        entity.setSubjectGameName(model.getSubjectGameName());
+
+        if (currentVersion != null) {
+            entity.setAuthorId(currentVersion.getAuthor().getUuid());
+            entity.setAuthorLogin(currentVersion.getAuthor().getLogin());
+            entity.setArticleName(currentVersion.getArticleName());
+            entity.setArticleContent(currentVersion.getArticleContent());
+            entity.setVersion(currentVersion.getVersionNumber());
+            entity.setCreatedAt(currentVersion.getCreatedAt());
+        }
+
+        return entity;
+    }
+
+    public GameArticleVersionEntity fromVersionModelToEntity(GameArticleVersionModel model) {
+        GameArticleVersionEntity entity = new GameArticleVersionEntity();
+        entity.setUuid(model.getUuid());
+        entity.setGameId(model.getGame().getUuid());
+        entity.setAuthorId(model.getAuthor().getUuid());
+        entity.setAuthorLogin(model.getAuthor().getLogin());
+        entity.setArticleName(model.getArticleName());
+        entity.setArticleContent(model.getArticleContent());
+        entity.setVersion(model.getVersionNumber());
+        entity.setCreatedAt(model.getCreatedAt());
+        return entity;
+    }
+}
