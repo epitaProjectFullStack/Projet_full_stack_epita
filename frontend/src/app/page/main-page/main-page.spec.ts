165573266648d7d6929b7689ed7da83d6b31f09e
Romain Vidal <romain1.vidal@epita.fr>
2026-03-22 18:15:49 +0100

    fix: Add mocking to main page test

--- frontend/src/app/page/main-page/main-page.spec.ts
+++ frontend/src/app/page/main-page/main-page.spec.ts
@@ -1,5 +1,9 @@
+import {signal, WritableSignal} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {provideRouter} from '@angular/router';
+
+import {Game} from '../../interface/game';
+import {BackendService} from '../../services/backend-service';
 
 import {MainPage} from './main-page';
 
@@ -7,10 +11,29 @@ describe('MainPage', () => {
   let component: MainPage;
   let fixture: ComponentFixture<MainPage>;
 
+  class BackendServiceStub {
+    public gamesList: WritableSignal<Game[]> = signal([]);
+
+    getAllGames(): void {
+      this.gamesList.set([{
+        uuid: '0000-0000-0000-0000',
+        authorLogin: 'oui',
+        subjectGameName: 'LoL',
+        articleName: 'Magic',
+        articleContent: 'Magic is magic!'
+      }])
+    }
+  }
+
   beforeEach(async () => {
     await TestBed
-        .configureTestingModule(
-            {imports: [MainPage], providers: [provideRouter([])]})
+        .configureTestingModule({
+          imports: [MainPage],
+          providers: [
+            provideRouter([]),
+            {provide: BackendService, useClass: BackendServiceStub}
+          ]
+        })
         .compileComponents();
 
     fixture = TestBed.createComponent(MainPage);
