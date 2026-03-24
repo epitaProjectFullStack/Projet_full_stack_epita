e00286673102a9728d9970708d5d873385ec5eb9
SunnyBara <sunny.soyer@epita.fr>
2026-03-24 16:50:01 +0100

    fix: resolve issue that asking for add UUID in UserResponse

--- backend/src/main/java/fr/epita/backend/controller/api/response/UserResponses/UserResponse.java
+++ backend/src/main/java/fr/epita/backend/controller/api/response/UserResponses/UserResponse.java
@@ -1,10 +1,13 @@
 package fr.epita.backend.controller.api.response.UserResponses;
 
+import java.util.UUID;
+
 import fr.epita.backend.utils.Role;
 import lombok.Data;
 
 @Data
 public class UserResponse {
+    private UUID id;
     private String login;
     private String mail;
     private Role role;
